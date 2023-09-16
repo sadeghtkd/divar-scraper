56ca4009e5529be34b15cb4342c5dc202aa49310
admin <sadegh.tkd@gmail.com>
2023-09-16 17:44:30 +0330

    fix retry request

--- DivaarScrape.py
+++ DivaarScrape.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
-query = "برلیانس H320"
+query = "رانا"
 
 def convert_fa_nums(value: str) -> str:
     return value.replace("۰","0").replace("۱", "1").replace("۲", "2").replace("۳", "3").replace("۴", "4").replace("۵", "5").replace("۶", "6").replace("۷", "7").replace("۸", "8").replace("۹", "9")
@@ -16,46 +16,46 @@ def clean_num(value:str) :
         return value
 
 def get_ads(page):
-    try:
-        url = "https://api.divar.ir/v8/web-search/3/light"
-        payload = '{"page":' + \
-            str(page) + \
-            ',"json_schema":{"category":{"value":"light"},"query":"'+query+'","cities":["3"]},"last-post-date":0}'
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-            'Accept': 'application/json, text/plain, */*',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Content-Type': 'application/json; charset=utf-8',
-            'Connection': 'keep-alive',
-            'TE': 'trailers'
-        }
-        response = requests.request(
-            "POST", url, headers=headers, data=payload.encode('utf-8'))
-        js = json.loads(response.text)
-        if js != None:
-            return js
-        else:
-            print("None returned in get ads, retring...")
+    success = False
+    while not success:
+        try:
+            url = "https://api.divar.ir/v8/web-search/3/light"
+            payload = '{"page":' + \
+                str(page) + \
+                ',"json_schema":{"category":{"value":"light"},"query":"'+query+'","cities":["3"]},"last-post-date":0}'
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+                'Accept': 'application/json, text/plain, */*',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json; charset=utf-8',
+                'Connection': 'keep-alive',
+                'TE': 'trailers'
+            }
+            response = requests.request(
+                "POST", url, headers=headers, data=payload.encode('utf-8'))
+            js = json.loads(response.text)
+            success = response.ok
+            if response.ok:
+                return js
+        except:
+            print("exception occured retring...")
             get_ads(page)
-    except:
-        print("exception occured retring...")
-        get_ads(page)
 
 
 def get_ad_details(ad_token):
-    try:
-        response = requests.get(
-            f'https://api.divar.ir/v8/posts-v2/web/{post_data["token"]}')
-        js = json.loads(response.text)
-        if js != None:
-            return js
-        else:
-            print("None returned in get ad details, retring...")
-            get_ad_details(ad_token)
-    except:
-        print("exception occured in get ad details, retring...")
-        get_ad_details(ad_token)
+    success = False
+    while not success:
+        try:
+            response = requests.get(
+                f'https://api.divar.ir/v8/posts-v2/web/{post_data["token"]}')
+            js = json.loads(response.text)
+            success = response.ok
+            if response.ok:
+                return js
+        except:
+            print("exception occured in get ad details, retring...")
+            
 
 page_count = int(input("Enter page count to fetch:"))
 page = 1
